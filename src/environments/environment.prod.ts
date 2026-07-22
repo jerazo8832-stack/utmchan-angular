f8c35fa0154b78ecab4ec00c46d866fd0686ad49
jerazo8832 <jerazo8832@utm.edu.ec>
2026-07-22 01:32:31 -0500

    fix: colocar URL real de Render en environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // 👉 IMPORTANTE: reemplaza esto por la URL real de tu backend en Render
   // Ejemplo: 'https://utmchan-backend.onrender.com/api'
-  apiUrl: 'https://TU-BACKEND-EN-RENDER.onrender.com/api'
+  apiUrl: 'https://utmchan-node.onrender.com/api'
 };
